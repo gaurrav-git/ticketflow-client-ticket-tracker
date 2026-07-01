2de93d77bc3153d678738014c9afba6543ac2eef
Gaurav Arora <gaurrav.git@gmail.com>
2026-07-01 23:28:16 +0530

    feat(database): create users table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE ticketflow;
 
 USE ticketflow;
 
-/*Lookup Table for Roles*/
+--Lookup Table for Roles
 
 CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,7 +19,7 @@ CREATE TABLE roles (
         ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*Lookup Table for Ticket Statuses*/
+--Lookup Table for Ticket Statuses
 
 CREATE TABLE ticket_statuses (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -34,7 +34,7 @@ CREATE TABLE ticket_statuses (
         ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*Lookup Table for Ticket Priorities*/
+--Lookup Table for Ticket Priorities
 
 CREATE TABLE ticket_priorities (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -49,7 +49,7 @@ CREATE TABLE ticket_priorities (
         ON UPDATE CURRENT_TIMESTAMP
 );
 
-/*Lookup Table for Ticket Categories*/
+--Lookup Table for Ticket Categories
 
 CREATE TABLE ticket_categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -62,6 +62,38 @@ CREATE TABLE ticket_categories (
 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Users
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    uuid CHAR(36) NOT NULL UNIQUE,
+
+    role_id INT NOT NULL,
+
+    first_name VARCHAR(100) NOT NULL,
+
+    last_name VARCHAR(100) NOT NULL,
+
+    email VARCHAR(255) NOT NULL UNIQUE,
+
+    password_hash VARCHAR(255) NOT NULL,
+
+    phone_number VARCHAR(20),
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_users_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
 );
 
 
